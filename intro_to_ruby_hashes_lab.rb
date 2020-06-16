e4250283d6f7d4855f9e0266930f6e9afa53d3a8
Haley Ferro <haleyvferro@gmail.com>
2020-06-16 00:08:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,17 +17,17 @@ def monopoly_with_second_tier
   #{ name: "Prince Escalus" },
   #{ name: "Apothecary" }
 #]
-first_hash[:pieces] = 
-{4}
+first_hash[:railroads] = 
+{ pieces: "4"}
 ]
-first_hash[:rent_in_dollars] = 
-{
+first_hash[:railroads] = 
+{ rent_in_dollars:
   
 }
 
 ]
-first_hash[:names] = 
-{
+first_hash[:railroads] = 
+{ names:
   
 }
 ]
