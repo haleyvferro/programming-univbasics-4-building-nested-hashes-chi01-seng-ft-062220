92124e2d318c6436fb5e705b06818e2b9fc85d12
Haley Ferro <haleyvferro@gmail.com>
2020-06-16 00:16:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,11 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  first_hash = [
+  first_hash = {
     :railroads => {
       
     }
-  ]
+  }
 end
 
 def monopoly_with_second_tier
