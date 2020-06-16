eaf51ce5a286b630572340f0deb03fba2d8e2ff9
Haley Ferro <haleyvferro@gmail.com>
2020-06-16 00:26:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base_hash = {
-    :railroads => {
-      :pieces =>
+    :railroads => 
+    { :pieces =>
     }
   }
 end
