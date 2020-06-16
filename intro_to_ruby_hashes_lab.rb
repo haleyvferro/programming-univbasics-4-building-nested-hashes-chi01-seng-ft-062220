245d827862d53b5dc5d4d75e3ce91d5f06712588
Haley Ferro <haleyvferro@gmail.com>
2020-06-16 00:29:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_second_tier
   # complexity.
   base_hash = {
     :railroads => 
-    { :pieces => "4", :rent_in_dollars => "", :names => ""
+    { :pieces => 4, :rent_in_dollars => "", :names => ""
     }
   }
 end
