e4a7920a2c03255349c44697dd94444108253e3a
Haley Ferro <haleyvferro@gmail.com>
2020-06-16 00:13:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,17 +13,12 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
- # epic_tragedy[:additional_characters] = [
-  #{ name: "Prince Escalus" },
-  #{ name: "Apothecary" }
-#]
 
 first_hash[:railroads] = [
-:pieces => {"4"}
-:rent_in_dollars => {}
-:names => {}
+:pieces => { 4 }
+:rent_in_dollars => { }
+:names => { }
 ]
-first_hash
 end
 
 def monopoly_with_third_tier
